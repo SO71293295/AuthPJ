0a6be8ffe1f0d514ddda12982f3698e8ce20e9df
shigeyuki_o@nu <98033101+SO71293295@users.noreply.github.com>
2022-03-13 14:45:42 +0000

    Update WebSecurityConfig.java

--- src/main/demo/config/WebSecurityConfig.java
+++ src/main/demo/config/WebSecurityConfig.java
@@ -14,46 +14,46 @@ import com.example.demo.service.AccountUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // メソッド認可処理を有効化
+@EnableGlobalMethodSecurity(prePostEnabled = true) // Enable method authorisation process
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AccountUserDetailsService userDetailsService;
 
     PasswordEncoder passwordEncoder() {
-        //BCryptアルゴリズムを使用してパスワードのハッシュ化を行う
-        return new BCryptPasswordEncoder(); // BCryptアルゴリズムを使用
+        //Hash of passwords using the BCrypt algorithm.
+        return new BCryptPasswordEncoder(); // Use of the BCrypt algorithm.
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // AuthenticationManagerBuilderに、実装したUserDetailsServiceを設定する
-        auth.userDetailsService(userDetailsService)     // 作成したUserDetailsServiceを設定
-                .passwordEncoder(passwordEncoder());    // パスワードのハッシュ化方法を指定(BCryptアルゴリズム)
+        // Set the implemented UserDetailsService in the AuthenticationManagerBuilder
+        auth.userDetailsService(userDetailsService)     // Configure the created UserDetailsService
+                .passwordEncoder(passwordEncoder());    // Specify password hashing method (BCrypt algorithm)
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 認可の設定
+        // Set authorisation
         http.exceptionHandling()
-                .accessDeniedPage("/accessDeniedPage")  // アクセス拒否された時に遷移するパス
+                .accessDeniedPage("/accessDeniedPage")  // Password to transition when access is denied
             .and()
             .authorizeRequests()
-                .antMatchers("/loginForm").permitAll()  // /loginFormは、全ユーザからのアクセスを許可
-                .anyRequest().authenticated();          // /loginForm以外は、認証を求める
+                .antMatchers("/loginForm").permitAll()  // /loginForm allows access from all users
+                .anyRequest().authenticated();          // /Except for loginForm, which requires authentication
 
         // ログイン設定
-        http.formLogin()                                // フォーム認証の有効化
-                .loginPage("/loginForm")                // ログインフォームを表示するパス
-                .loginProcessingUrl("/authenticate")    // フォーム認証処理のパス
-                .usernameParameter("userName")          // ユーザ名のリクエストパラメータ名
-                .passwordParameter("password")          // パスワードのリクエストパラメータ名
-                .defaultSuccessUrl("/home")             // 認証成功時に遷移するデフォルトのパス
-                .failureUrl("/loginForm?error=true");   // 認証失敗時に遷移するパス
+        http.formLogin()                                // Enable form authentication
+                .loginPage("/loginForm")                // Password to display login form
+                .loginProcessingUrl("/authenticate")    // Password of the form authentication process
+                .usernameParameter("userName")          // Username of request parameter name
+                .passwordParameter("password")          // Password of request parameter name
+                .defaultSuccessUrl("/home")             // Default password to transition to on successful authentication
+                .failureUrl("/loginForm?error=true");   // Password to be transitioned to in the event of authentication failure
 
         // ログアウト設定
         http.logout()
-                .logoutSuccessUrl("/loginForm")         // ログアウト成功時に遷移するパス
-                .permitAll();                           // 全ユーザに対してアクセスを許可
+                .logoutSuccessUrl("/loginForm")         // Password to be transitioned to on successful logout
+                .permitAll();                           // Access allows to all users
     }
 }
